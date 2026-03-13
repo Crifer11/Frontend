7e43178b5f9564e8192ce679351cdbe4e3199750
Crifer11 <kenneth.chavez@alumnos.udg.mx>
2026-03-12 22:16:38 -0600

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -234,7 +234,7 @@ function Login() {
       {/* 🔹 LOGO */}
       <Box
         component="img"
-        src={`${API_URL}/logo2.png`}
+        src="../../logo2.png"
         alt="SafeGate Logo"
         sx={{
           width: 250,
